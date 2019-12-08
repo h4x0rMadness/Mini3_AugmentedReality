ca0283ec6c634932aeea96bef2fd2925ed4d45b3
h4x0rMadness <robertmorrislike@gmail.com>
2019-12-08 00:32:45 -0500

    Add files via upload

---
+++ TouchCube/source/InputManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public GameObject cubePrefab;
+
+    void Update()
+    {
+      foreach(var t in Input.touches){
+        if(t.phase != TouchPhase.Began) continue;
+        var ray = Camera.main.ScreenPointToRay(t.position);
+        RaycastHit hitInfo;
+        if(Physics.Raycast(ray, out hitInfo)){
+          var go = GameObject.Instantiate(cubePrefab, hitInfo.point + Vector3.up,Quaternion.identity);
+          go.GetComponent<MeshRenderer>().material.color = Random.ColorHSV();
+        }
+      }  
+    }
+}
